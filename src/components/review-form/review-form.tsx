8368ff427df95dffaf0d090cb27e480234a3bb0b
Technopriest90 <erokhin.ag@yandex.ru>
2023-07-19 12:36:51 +0300

    Правка замечаний: Сделал компонент rating отдельно

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { Review } from '../../types/review';
+import Rating from '../rating/rating';
 
 type ReviewFormProps = {
   addReview: (review: Review) => void;
@@ -20,18 +21,17 @@ const formState: Review = {
 export default function ReviewForm({ addReview }: ReviewFormProps) {
   const [formData, setFormData] = useState(formState);
   const { comment, rating } = formData;
-  const handleChangeRating = ({ target: { defaultValue } }: ChangeEvent<HTMLInputElement>) => {
-    setFormData((prev) => ({ ...prev, rating: +defaultValue }));
+
+  const setRating = (value: number) => {
+    setFormData((prev) => ({ ...prev, rating: value }));
   };
   const handleChangeComment = ({ target: { value } }: ChangeEvent<HTMLTextAreaElement>) => {
     setFormData((prev) => ({ ...prev, comment: value }));
   };
   const handleSubmitReview = ((evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    setFormData((prev) => {
-      addReview({...prev, date: new Date().toISOString()});
-      return ({...prev, id: crypto.randomUUID(), comment: '', rating: 0 });
-    });
+    addReview({...formData, date: new Date().toISOString()});
+    setFormData((prev) => ({...prev, id: crypto.randomUUID(), comment: '', rating: 0 }));
   });
   return (
     <form
@@ -43,98 +43,7 @@ export default function ReviewForm({ addReview }: ReviewFormProps) {
       <label className="reviews__label form__label" htmlFor="review">
         Your review
       </label>
-      <div className="reviews__rating-form form__rating">
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          defaultValue={5}
-          id="5-stars"
-          type="radio"
-          checked={rating === 5}
-          onChange={handleChangeRating}
-        />
-        <label
-          htmlFor="5-stars"
-          className="reviews__rating-label form__rating-label"
-          title="perfect"
-        >
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          defaultValue={4}
-          id="4-stars"
-          type="radio"
-          checked={rating === 4}
-          onChange={handleChangeRating}
-        />
-        <label
-          htmlFor="4-stars"
-          className="reviews__rating-label form__rating-label"
-          title="good"
-        >
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          defaultValue={3}
-          id="3-stars"
-          type="radio"
-          checked={rating === 3}
-          onChange={handleChangeRating}
-        />
-        <label
-          htmlFor="3-stars"
-          className="reviews__rating-label form__rating-label"
-          title="not bad"
-        >
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          defaultValue={2}
-          id="2-stars"
-          type="radio"
-          checked={rating === 2}
-          onChange={handleChangeRating}
-        />
-        <label
-          htmlFor="2-stars"
-          className="reviews__rating-label form__rating-label"
-          title="badly"
-        >
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-        <input
-          className="form__rating-input visually-hidden"
-          name="rating"
-          defaultValue={1}
-          id="1-star"
-          type="radio"
-          checked={rating === 1}
-          onChange={handleChangeRating}
-        />
-        <label
-          htmlFor="1-star"
-          className="reviews__rating-label form__rating-label"
-          title="terribly"
-        >
-          <svg className="form__star-image" width={37} height={33}>
-            <use xlinkHref="#icon-star" />
-          </svg>
-        </label>
-      </div>
+      <Rating rating={rating} setRating={setRating} />
       <textarea
         className="reviews__textarea form__textarea"
         id="review"
