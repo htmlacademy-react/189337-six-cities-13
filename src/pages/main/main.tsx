326805ba0161e4d16ca9f393d9f30d8ad8b03fdf
Technopriest90 <erokhin.ag@yandex.ru>
2023-07-23 12:46:39 +0300

    добавил sorting-menu

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,6 +4,7 @@ import CardList from '../../components/card-list/card-list';
 import { Offer } from '../../types/offers';
 import TabBar from '../../components/tab-bar/tab-bar';
 import { Settings, TAB_BAR_CITIES } from '../../const';
+import SortingMenu from '../../components/sorting-menu/sorting-menu';
 
 type MainScreenProps = {
   offers: Offer [];
@@ -24,21 +25,7 @@ export default function Main({ offers }: MainScreenProps): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <SortingMenu />
               <CardList classNameWrapper="cities__places-list  places__list tabs__content" classNameCardPrefix="cities" offers={offers}/>
             </section>
             <div className="cities__right-section">
